ba35da26ad33bd9465ae49d6ecdb4a4c10d57aaf
Darsh Thakur <thakurdarsh21@gmail.com>
2026-03-11 23:48:38 +0530

    feat: auth service setup

---
+++ src/test/java/com/uws/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uws.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
